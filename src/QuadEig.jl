7b1f0b77be754d066aac0fa96cace81d616fdb2e
Pablo San-Jose <lekand@gmail.com>
2021-03-23 11:33:40 +0100

    License + Project.toml update, etc

--- src/QuadEig.jl
+++ src/QuadEig.jl
@@ -1,18 +1,18 @@
 module QuadEig
 
-using LinearAlgebra, SparseArrays
+using LinearAlgebra, SparseArrays, SuiteSparse
 
 ### QuadPencil
 # Encodes the quadratic pencil problem
 ###
-struct QuadPencil{T>:Complex,M<:AbstractMatrix{T}}
+struct QuadPencil{T<:Complex,M<:AbstractMatrix{T}}
     A0::M
     A1::M
     A2::M
 end
 
 function quadpencil(A0, A1, A2)
-    size(A0) == size(A1) == size(A2) && issquare(A0) ||
+    size(A0) == size(A1) == size(A2) && size(A0, 1) == size(A0, 2) ||
         throw(DimensionMismatch("Expected square matrices of the same size"))
     ptype = complex(promote_type(eltype(A0), eltype(A1), eltype(A2)))
     A0´ = unwrap_adjoint(A0, ptype)
@@ -36,13 +36,13 @@ struct QuadPencilPQR{T,M,Q<:Factorization{T}}
     qr2::Q
 end
 
-function quadpencilPQR(pencil::QuadPencil{T}; kw...) where {T}
-    qr0 = pqr!(copy(q.A0))
-    qr2 = pqr!(copy(q.A2))
-    return QuadPencilQR(pencil, qr0, qr2)
+function quadpencilPQR(pencil::QuadPencil{T}) where {T}
+    qr0 = pqr!(copy(pencil.A0))
+    qr2 = pqr!(copy(pencil.A2))
+    return QuadPencilPQR(pencil, qr0, qr2)
 end
 
-pqr!(a::SparseMatrix) = qr!(a)
+pqr!(a::SparseMatrixCSC) = qr(a)
 pqr!(a) = qr!(a, Val(true))
 
 ### LinearizedPencil
@@ -54,11 +54,12 @@ struct LinearizedPencil{T,M<:AbstractMatrix{T}}
     V::M
 end
 
-function linearizedpencil(p::QuadPencil)
+function linearizedpencil(p::QuadPencil{T,M}) where {T,M}
     n = size(p, 1)
-    A = [p.A1 -I(n); p.A0 0I(n)]
-    B = [-p.A2 0I(n); 0I(n) -I(n)]
-    V = [I(n) 0I(n); 0I(n) I(n)]
+    o, z = one(p.A1), zero(p.A1)
+    A = [p.A1 -o; p.A0 z]
+    B = [-p.A2 z; z -o]
+    V = M(I, 2n, 2n)
     return LinearizedPencil(A, B, V)
 end
 
@@ -66,7 +67,7 @@ function linearizedpencil(q::QuadPencilPQR{T}) where {T}
     A0, A1, A2 = q.pencil.A0, q.pencil.A1, q.pencil.A2
     o, z = one(A1), zero(A1)
     Q0, Q2´ = getQ(q.qr0), getQ´(q.qr2)
-    RP0, RP2 = getRP´(qrA0), getRP´(qrA2)
+    RP0, RP2 = getRP´(q.qr0), getRP´(q.qr2)
     V = [o z; z Q0]
     A = [Q2´*A0 -Q2´*Q0; RP0 z]
     B = [RP2 z; z o]
@@ -74,23 +75,11 @@ function linearizedpencil(q::QuadPencilPQR{T}) where {T}
     return LinearizedPencil(A, B, V)
 end
 
-function linearizedpencil(q::QuadPencilPQR{T,<:SparseMatrix}) where {T}
-    A0, A1, A2 = q.pencil.A0, q.pencil.A1, q.pencil.A2
-    o, z = one(A1), zero(A1)
-    Q0, Q2 = q.qr0.Q, q.qr2.Q
-    RP0, RP2 = qrA0.R * qrA0.P', qrA2.R * qrA2.P'
-    V = [o z; z Q0]
-    A = [Q2'A0 -Q2'Q0; RP0 z]
-    B = [RP2 z; z o]
-    B .= .- B
-    return LinearizedPencil(A, B, V)
-end
-
 getQ(qr::QRPivoted) = qr.Q
-getQ(qr::QRSuiteSparse.SPQR.QRSparse) =  I(size(qr,1))[:, qr.prow] * sparse(Matrix(qr.Q))
+getQ(qr::SuiteSparse.SPQR.QRSparse) =  I(size(qr,1))[:, qr.prow] * sparse(Matrix(qr.Q))
 
 getQ´(qr::QRPivoted) = qr.Q'
-getQ´(qr::QRSuiteSparse.SPQR.QRSparse) = sparse(Matrix(qr.Q)') * I(size(qr,1))[qr.prow,:]
+getQ´(qr::SuiteSparse.SPQR.QRSparse) = sparse(Matrix(qr.Q)') * I(size(qr,1))[qr.prow,:]
 
 
 getRP´(qr::QRPivoted) = qr.R * qr.P'
@@ -104,7 +93,7 @@ Base.size(l::LinearizedPencil, n...) = size(l.A, n...)
 ### deflate
 # compute deflated pencil
 ###
-function deflate(l::LinearizedPencil{T}, atol = sqrt(real(eps(T))))
+function deflate(l::LinearizedPencil{T}, atol = sqrt(eps(real(T)))) where {T}
     rank0, rank2 = nonzero_rows(l, atol)
     rank0 == rank2 || throw(ArgumentError("The case with inhomogeneous ranks not yet supported"))
     r = rank0
@@ -113,24 +102,29 @@ function deflate(l::LinearizedPencil{T}, atol = sqrt(real(eps(T))))
     QX, ZX = qz(X)
 end
 
-function nonzero_rows(l::LinearizedPencil{T}, atol = sqrt(real(eps(T)))) where {T}
+function nonzero_rows(l::LinearizedPencil{T}, atol = sqrt(eps(real(T)))) where {T}
     n = size(l, 2) ÷ 2
-    rank0 = 0
-    for row in n+1:2n
-        all(z -> abs(z) < atol, view(l.A, row, 1:n)) && break
-        rank0 += 1
-    end
-    rank2 = 0
-    for row in 1:n
-        all(z -> abs(z) < atol, view(l.B, row, 1:n)) && break
-        rank0 += 1
-    end
+    rank0, rank2 = nonzero_rows(view(l.A, n+1:2n, 1:n), atol), nonzero_rows(view(l.B, 1:n, 1:n), atol)
     return rank0, rank2
 end
 
-qz(X::SubArray{T,2,<:SparseMatrix}) = qz(sparse(X))
+function nonzero_rows(p::QuadPencilPQR{T}, atol = sqrt(eps(real(T)))) where {T}
+    rank0, rank2 = nonzero_rows(p.qr0.R, atol), nonzero_rows(p.qr2.R, atol)
+    return rank0, rank2
+end
 
-function qz(X::SparseMatrix)
+function nonzero_rows(m::AbstractMatrix{T}, atol = sqrt(eps(real(T)))) where {T}
+    n = 0
+    for row in eachrow(m)
+        all(z -> abs(z) < atol, row) && break
+        n += 1
+    end
+    return n
+end
+
+qz(X::SubArray{<:Complex,2,<:SparseMatrixCSC}) = qz(sparse(X))
+
+function qz(X::SparseMatrixCSC)
     qrX = qr(X)
     PR´ = getPR´(qrX)
     lq´ = qr(PR´)
@@ -141,10 +135,11 @@ end
 
 function qz(X)
     qrX = qr(X, Val(true))
-    PR = getRP´(qrX)
-    lq = lq(PR)
-    Q = Matrix(qrX)
-    Z = lq.Z * Matrix(I, size(X, 2), size(X, 2))
+    RP = getRP´(qrX)
+    lqR = lq(RP)
+    Q = qrX.Q * Matrix(I, size(X, 1), size(X, 1))
+    Z = lqR.Z * Matrix(I, size(X, 2), size(X, 2))
     return Q, Z
 end
 
+end
